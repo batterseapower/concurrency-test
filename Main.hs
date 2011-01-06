f08b705fc296726c62acef97016855c6fb8b6680
Max Bolingbroke <batterseapower@hotmail.com>
2011-01-06 20:01:32 +0000

    Prepare for RTSVars by threading ST everywhere (this is easier than solving the typeing problem for RTSVars by myself)

--- Main.hs
+++ Main.hs
@@ -2,26 +2,35 @@
 
 import Control.Monad
 
-
-newtype Pending b = Pending { unPending :: [Pending b] -> b }
-newtype RTSM a = RTSM { unRTSM :: forall b. (a -> Pending b) -> Pending b }
-
-runRTSM :: RTSM a -> a
-runRTSM mx = unPending (unRTSM mx (\x -> Pending $ \_pendings -> x)) []
+import Control.Monad.ST
+import Data.STRef
 
 
-instance Functor RTSM where
+newtype Pending s b = Pending { unPending :: [Pending s b] -> ST s b }
+newtype RTSM s a = RTSM { unRTSM :: forall b. (a -> Pending s b) -> Pending s b }
+
+runRTSM :: (forall s. RTSM s a) -> a
+runRTSM mx = runST (unPending (unRTSM mx (\x -> Pending $ \_pendings -> return x)) [])
+
+
+instance Functor (RTSM s) where
     fmap = liftM
 
-instance Monad RTSM where
+instance Monad (RTSM s) where
     return x = RTSM $ \k -> k x
     mx >>= fxmy = RTSM $ \k_y -> Pending $ \pendings -> unPending (unRTSM mx (\x -> unRTSM (fxmy x) k_y)) pendings
 
-yield :: RTSM ()
+yield :: RTSM s ()
 yield = RTSM $ \k -> Pending $ \pendings -> case schedule (k (), pendings) of (k, pendings) -> unPending k pendings
 
-schedule :: (Pending b, [Pending b]) -> (Pending b, [Pending b])
+schedule :: (Pending s b, [Pending s b]) -> (Pending s b, [Pending s b])
 schedule (x, xs) = (last (x:xs), init (x:xs)) -- Round robin scheduling (poor mans queue)
+
+
+newtype RTSVar s a = RTSVar { unRTSVar :: STRef s (Maybe a) }
+
+newEmptyRTSVar :: RTSM s (RTSVar s a)
+newEmptyRTSVar = undefined
 
 
 main :: IO ()
