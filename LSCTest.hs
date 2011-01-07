36cc3ce39404375bd07c2007a47079df4f217ebc
Max Bolingbroke <batterseapower@hotmail.com>
2011-01-07 21:03:26 +0000

    A good test of infinite data structures in LSCTest

--- LSCTest.hs
+++ LSCTest.hs
@@ -1,4 +1,7 @@
 import Control.Arrow (first)
+import Control.Exception
+import Control.Monad
+import System.IO.Unsafe
 
 import Test.LazySmallCheck
 
@@ -16,8 +19,24 @@ lengthAtLeast []     _ = False
 lengthAtLeast (x:xs) n = lengthAtLeast xs (n - 1)
 
 
+-- I used to use unsafeIsEvaluated to decide where to put in "...", but that pruned too heavily because
+-- I couldn't show the schedule before it was actually poked on and those thunks turned into real values.
+{-# NOINLINE showExplored #-}
+showExplored :: (a -> String) -> a -> String
+showExplored show x = unsafePerformIO $ fmap (maybe "..." show) $ tryIf isLSCError (evaluate x)
+  where
+    -- Looked at the LSC code to see what sort of errors it was generating...
+    isLSCError (ErrorCall ('\0':msg)) = True
+    isLSCError _                      = False
+
+    tryIf :: Exception e => (e -> Bool) -> IO a -> IO (Maybe a)
+    tryIf p act = fmap (either (\() -> Nothing) Just) $ tryJust (\e -> guard (p e) >> return ()) act
+
+
 data Stream a = a :< Stream a
-              deriving (Show)
+
+instance Show a => Show (Stream a) where
+    show = showExplored $ \(x :< xs) -> show x ++ " :< " ++ show xs
 
 instance Serial a => Serial (Stream a) where
     series = cons2 (:<)
@@ -33,9 +52,12 @@ streamPrepend (x:xs) ys = x :< streamPrepend xs ys
 streamAt :: Stream a -> Int -> a
 streamAt (x :< xs) n = if n == 0 then x else streamAt xs (n - 1)
 
-prop_stream :: Stream Int -> Bool
-prop_stream xs = (((before ++ [9]) `streamPrepend` after) `streamAt` 4) == 9
+prop_stream1 :: Stream Int -> Bool
+prop_stream1 xs = (((before ++ [9]) `streamPrepend` after) `streamAt` 4) == 9
   where (before, after) = splitStream 4 xs
+
+prop_stream2 :: Stream Bool -> Bool
+prop_stream2 (x :< _) = x == True
 
 
 data Streem a = Streem a (Stream (Streem a))
@@ -50,5 +72,6 @@ instance Serial a => Serial (Streem a) where
 
 main :: IO ()
 main = do
-    test prop_stream
+    --test prop_stream1
+    test prop_stream2
     --test prop_fun
