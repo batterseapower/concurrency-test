f9b5271de9772a9188f1fb0da69836c8f92c3fbe
Max Bolingbroke <batterseapower@hotmail.com>
2011-01-08 22:06:12 +0000

    Move MutableQueue to Data.STQueue

--- Data/STQueue.hs
+++ Data/STQueue.hs
@@ -1,3 +1,8 @@
+module Data.STQueue (
+    STQueue, Location,
+    new, enqueue, dequeue, delete
+  ) where
+
 import Control.Monad.ST
 import Data.STRef
 
@@ -9,16 +14,16 @@ data Node s a = Node { location :: Integer, value :: a, backwards_ref :: STRef s
 
 data Front s a = Empty
                | NonEmpty (Node s a) (Node s a)
-data MutableQueue s a = MQ { next_location_ref :: STRef s Integer, front_ref :: STRef s (Front s a) }
+data STQueue s a = MQ { next_location_ref :: STRef s Integer, front_ref :: STRef s (Front s a) }
                       deriving (Eq)
 
-new :: ST s (MutableQueue s a)
+new :: ST s (STQueue s a)
 new = do
     next_location_ref <- newSTRef 0
     front_ref <- newSTRef Empty
     return $ MQ next_location_ref front_ref
 
-enqueue :: a -> MutableQueue s a -> ST s (Location s a)
+enqueue :: a -> STQueue s a -> ST s (Location s a)
 enqueue x q = do
     next_location_i <- readSTRef (next_location_ref q)
     writeSTRef (next_location_ref q) (next_location_i + 1)
@@ -36,7 +41,7 @@ enqueue x q = do
     
     return $ L next_location_i q
 
-dequeue :: MutableQueue s a -> ST s (Maybe a)
+dequeue :: STQueue s a -> ST s (Maybe a)
 dequeue q = do
     mb_front <- readSTRef (front_ref q)
     case mb_front of
@@ -46,11 +51,11 @@ dequeue q = do
           replaceFront q mb_backwards back
           return (Just (value front))
 
-replaceFront :: MutableQueue s a -> Maybe (Node s a) -> Node s a -> ST s ()
+replaceFront :: STQueue s a -> Maybe (Node s a) -> Node s a -> ST s ()
 replaceFront q mb_front back = writeSTRef (front_ref q) (maybe Empty (\front -> NonEmpty front back) mb_front)
 
 
-data Location s a = L Integer (MutableQueue s a) -- TODO: could probably implement constant-time delete if I could tolerate more pointers
+data Location s a = L Integer (STQueue s a) -- TODO: could probably implement constant-time delete if I could tolerate more pointers
                   deriving (Eq)
 
 delete :: Location s a -> ST s (Maybe a)
@@ -70,6 +75,7 @@ delete (L location_i q) = do
         return (Just (value this))
 
 
+main :: IO ()
 main = print $ runST $ do
     q <- new
     enqueue 1 q
